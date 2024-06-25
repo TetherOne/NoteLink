32c109f33383780ee947b5dd876b15c9cbb8d7f5
Pasha Ryaguzov <pashajobber@gmail.com>
2024-06-25 17:53:44 +0500

    ad alembic

---
+++ alembic/versions/2024_06_25_1749-504377e11e9b_create_note_model.py
@@ -0,0 +1,45 @@
+"""create note model
+
+Revision ID: 504377e11e9b
+Revises:
+Create Date: 2024-06-25 17:49:29.418828
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "504377e11e9b"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "notes",
+        sa.Column("title", sa.String(), nullable=False),
+        sa.Column("text", sa.String(), nullable=False),
+        sa.Column("expire", sa.DateTime(), nullable=False),
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column(
+            "created_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+        sa.Column(
+            "updated_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("notes")
