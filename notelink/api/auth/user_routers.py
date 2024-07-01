b3b17493a795c8eadf9a3d01738d9f64917f5792
Pasha Ryaguzov <pashajobber@gmail.com>
2024-07-01 20:15:12 +0500

    create user_router

---
+++ notelink/api/auth/user_routers.py
@@ -0,0 +1,11 @@
+from fastapi_users import FastAPIUsers
+
+from .backend import authentication_backend
+from .user_manager import get_user_manager
+from ...core.models import User
+from ...core.types.user_id import UserId
+
+fastapi_users = FastAPIUsers[User, UserId](
+    get_user_manager,
+    [authentication_backend],
+)
