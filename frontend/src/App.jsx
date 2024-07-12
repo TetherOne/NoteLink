756cfcd2b7731561e7b821daebf462fc6d9a7599
Pasha Ryaguzov <pashajobber@gmail.com>
2024-07-12 23:29:30 +0500

    added components folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
-function App() {
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8005/api/v1/notes/public/").then(
+      (response) => {
+      console.log(response.data);
+    });
+  }, []);
 
   return (
-    <>
-    </>
-  )
+    <div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    './components/**/*.{js,jsx,ts,tsx}',
-    './pages/**/*.{js,jsx,ts,tsx}',
-    './index.html',
-  ],
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-  corePlugins: {
-    preflight: false
-  },
 }
